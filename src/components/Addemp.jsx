c4a0c36c6f325ac048e7099da4bb9f5c8c3e6a6b
iPuppyYT <dhanush6102002@gmail.com>
2023-05-05 01:02:44 +0530

    Update Addemp.jsx

--- src/components/Addemp.jsx
+++ src/components/Addemp.jsx
@@ -9,66 +9,16 @@ const Addemp = () => {
   const GetVal = (val) => {
     axios.post("https://reqres.in/api/users", val).then((res) => {
       console.log(val);
-      
-      <Stack sx={{ width: '100%' }} spacing={2} className="alertbox">
-      <Alert variant="filled" severity="error">
-        This is an error alert — check it out!
-      </Alert>
-      </Stack>
+      alert(res.data.empname + " has been successfully added into employees.");
     });
   };
   return (
     <div>
-      <Stack
-        display={"block"}
-        marginLeft={"auto"}
-        marginRight={"auto"}
-        marginTop={20}
-        component="form"
-        sx={{
-          width: "25ch",
-        }}
-        spacing={2}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          hiddenLabel
-          name="empname"
-          {...register("empname")}
-          id="filled-hidden-label-small"
-          label="Employee Name"
-          variant="standard"
-          size="small"
-          fullWidth="100px"
-        />
-        <TextField
-          hiddenLabel
-          name="emplocation"
-          {...register("emplocation")}
-          id="filled-hidden-label-small"
-          label="Employee Location"
-          variant="standard"
-          size="small"
-          fullWidth="100px"
-        />
-        <TextField
-          hiddenLabel
-          name="empdescription"
-          {...register("empdescription")}
-          id="filled-hidden-label-normal"
-          label="Employee Description"
-          variant="standard"
-          size="small"
-          fullWidth="100px"
-        />
-        <Button
-          fullWidth="100px"
-          variant="contained"
-          onClick={handleSubmit(GetVal)}
-        >
-          Submit
-        </Button>
+      <Stack display={"block"} marginLeft={"auto"} marginRight={"auto"} marginTop={20} component="form" sx={{width: "25ch"}} spacing={2} noValidate autoComplete="off">
+        <TextField hiddenLabel name="empname" {...register("empname")} id="filled-hidden-label-normal" label="Employee Name" variant="standard" size="small"/>
+        <TextField hiddenLabel name="empdescription" {...register("empdescription")} id="filled-hidden-label-normal" label="Employee Description" variant="standard" size="small"/>
+        <TextField hiddenLabel name="emplocation" {...register("emplocation")} id="filled-hidden-label-normal" label="Employee Location" variant="standard" size="small"/>
+        <Button fullWidth="100px" variant="contained" onClick={handleSubmit(GetVal)}>Submit</Button>
       </Stack>
     </div>
   );
